595f14a4dd151c5776b5bcc9907e01f410a9449b
Michael Gomes Ferreira <michael.21.2012@hotmail.com>
2023-06-01 21:11:52 -0300

    add encrypter

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,19 @@
+import type { AccountModel } from '../../../domain/models/account'
+import type { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
+import type { Encrypter } from '../../protocols/encrypter'
+
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter
+
+  constructor (encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(account.password)
+    return await new Promise(resolve => {
+      /** @ts-expect-error for only test porpose in this moment */
+      resolve(null)
+    })
+  }
+}
