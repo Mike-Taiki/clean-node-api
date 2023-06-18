a378c40200959b402120717c9481048904ba478b
Michael Gomes Ferreira <michael.21.2012@hotmail.com>
2023-06-17 23:29:01 -0300

    feat: add mongodb repository

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'jest-environment-node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,17 @@
+import { type Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(global.__MONGO_URI__)
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  }
+}
