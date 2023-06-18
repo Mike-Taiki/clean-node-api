ee206bb8ef119cc99e12da263079c292e9953560
Michael Gomes Ferreira <michael.21.2012@hotmail.com>
2023-06-17 23:55:01 -0300

    refactor: add account

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -5,16 +5,9 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    try {
-      const accountCollection = MongoHelper.getCollection('accounts')
-      const result = await accountCollection.insertOne(accountData)
-      const account = await accountCollection.findOne({ _id: result.insertedId })
-      if (account?._id) {
-        return MongoHelper.map(account)
-      }
-      throw new Error('Error when saving account')
-    } catch (err) {
-      throw new Error(err)
-    }
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account = await accountCollection.findOne({ _id: result.insertedId })
+    return MongoHelper.map(account)
   };
 }
