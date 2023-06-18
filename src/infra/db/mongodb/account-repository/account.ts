8716b8452764a22d2de692346d5829ef32bba56e
Michael Gomes Ferreira <michael.21.2012@hotmail.com>
2023-06-17 23:50:33 -0300

    refactor: add helper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -4,14 +4,13 @@ import type { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
-  async add (accountData: AddAccountModel): Promise<AccountModel> {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
     try {
       const accountCollection = MongoHelper.getCollection('accounts')
       const result = await accountCollection.insertOne(accountData)
       const account = await accountCollection.findOne({ _id: result.insertedId })
       if (account?._id) {
-        const { _id, name, email, password } = account
-        return Object.assign({}, { id: _id.toString(), name, email, password })
+        return MongoHelper.map(account)
       }
       throw new Error('Error when saving account')
     } catch (err) {
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,15 +3,20 @@ import { type Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
   client: MongoClient,
 
-  async connect (uri: string): Promise<void> {
+  async connect(uri: string): Promise<void> {
     this.client = await MongoClient.connect(global.__MONGO_URI__)
   },
 
-  async disconnect (): Promise<void> {
+  async disconnect(): Promise<void> {
     await this.client.close()
   },
 
-  getCollection (name: string): Collection {
+  getCollection(name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map(collection: any): any {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id })
   }
 }
