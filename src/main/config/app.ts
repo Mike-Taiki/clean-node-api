0c26645276aea5ec4bd65d1f208d8ce012689a0d
Michael Gomes Ferreira <michael.21.2012@hotmail.com>
2023-06-19 21:59:13 -0300

    feat: add signup routes

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import setpeRoutes from './routes'
 
 const app = express()
 setupMiddlewares(app)
+setpeRoutes(app)
 export default app
